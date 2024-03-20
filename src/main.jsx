e42b0034c3cf11040af3b8058a90b722ca079317
sam-kim99 <samkim0451@gmail.com>
2024-03-20 15:57:15 -0400

    add produce to page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import App from './App';
 import './index.css';
+import { populateProduce } from './store/produce'
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== "production") {
   window.store = store;
+  window.populateProduce = populateProduce;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
